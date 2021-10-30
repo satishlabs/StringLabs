c5440041a16fed190d6fb92dede32cdf6eafcc92
Satish Prasad <spd61185@gmail.com>
2021-10-30 14:51:50 +0530

    String Immutable

---
+++ src/com/satishlabs/string/StringExamples1.java
@@ -0,0 +1,11 @@
+package com.satishlabs.string;
+
+public class StringExamples1 {
+	public static void main(String[] args) {
+		String s1="Java";
+		String s2="Java";
+		System.out.println(s1==s2);
+		s1 = s1+"J2ee";
+		System.out.println(s1==s2);
+	}
+}
